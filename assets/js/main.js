0bfc22035d0617d8816954f07972fa9a5923189c
jeromehardaway <jerome@vetswhocode.io>
2020-12-30 14:19:51 -0600

    completing the style of contact

--- assets/js/main.js
+++ assets/js/main.js
@@ -182,3 +182,25 @@
   });
 
 })(jQuery);
+
+//
+const btn = document.getElementById('button');
+
+document.getElementById('form')
+ .addEventListener('submit', function(event) {
+   event.preventDefault();
+
+   btn.value = 'Sending...';
+
+   const serviceID = 'default_service';
+   const templateID = 'jerome_email';
+
+   emailjs.sendForm(serviceID, templateID, this)
+    .then(() => {
+      btn.value = 'Send Email';
+      alert('Sent!');
+    }, (err) => {
+      btn.value = 'Send Email';
+      alert(JSON.stringify(err));
+    });
+});
